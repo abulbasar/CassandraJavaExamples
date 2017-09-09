aeb5e4c5e51db62e2e406aadbefa86234c0a2064
Abul Basar <abulbasar@users.noreply.github.com>
2017-09-09 23:40:24 +0530

    Update DataLoader.java

--- src/com/example/DataLoader.java
+++ src/com/example/DataLoader.java
@@ -1,93 +1,68 @@
-package main.scala
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
 
-import java.sql.Timestamp
+import com.datastax.driver.core.Cluster;
+import com.datastax.driver.core.LocalDate;
+import com.datastax.driver.core.PreparedStatement;
+import com.datastax.driver.core.Session;
+import com.univocity.parsers.csv.CsvParser;
+import com.univocity.parsers.csv.CsvParserSettings;
 
-import org.apache.spark.sql.Row
-import org.slf4j.LoggerFactory
 
-import com.datastax.driver.core.LocalDate
-import com.datastax.spark.connector.cql.CassandraConnector
-import org.apache.spark.sql.SaveMode
-import org.apache.spark.sql.{functions => Functions}
+public class DataLoader {
+	
+	private static Session session = null;
+	private static PreparedStatement loadStatement = null;
+		
+	public static List<String[]> readData(String path) throws Exception {
 
-/*
- *  create table stocks(date date,open double,high double,low double,close double, 
- *  volume double,adjclose double,symbol text, primary key (date, symbol));
- * 
- * */
+		CsvParserSettings settings = new CsvParserSettings();
+		settings.getFormat().setLineSeparator("\n");
+		CsvParser parser = new CsvParser(settings);
+		InputStream inputStream = new FileInputStream(path);
+		
+		InputStreamReader inputStreamReader = new InputStreamReader(inputStream, "UTF-8");
+		List<String[]> allRows = parser.parseAll(inputStreamReader);
 
-case class Stock(date:Timestamp,open:Double,high:Double,
-    low:Double,close:Double,volume:Double,adjclose:Double,symbol:String)
-
-object DataLoader {
-  
-  private val logger = LoggerFactory.getLogger(getClass)
-
-  def loadAs(file: String, table: String, cache: Boolean = false) {
-    val options = Map("header" -> "true","inferSchema" -> "true")
-    spark.read.format("csv").options(options).load(file).createOrReplaceTempView(table)
-    if (cache) {
-      spark.table(table).cache()
-    }
-  }
-  
-  def saveEachPartition(rows:Iterator[Stock]) = {
-    val cassandraConnector = CassandraConnector.apply(sparkConf)
-    val cassandraSession = cassandraConnector.openSession() 
-    val statement = cassandraSession.prepare("insert into demo.stocks (date,open,high,low,close,volume,adjclose,symbol) values (?,?,?,?,?,?,?,?)")
-    
-    val startTime = System.currentTimeMillis()
-   
-    val results = rows.toArray.map{row => 
-        val localDate = LocalDate.fromMillisSinceEpoch(row.date.getTime)
-        
-        val boundStatement = statement.bind()
-        boundStatement.setDate("date", localDate)
-        boundStatement.setString("symbol", row.symbol)
-        boundStatement.setDouble("open", row.open)
-        boundStatement.setDouble("high", row.high)
-        boundStatement.setDouble("close", row.close)
-        boundStatement.setDouble("low", row.low)
-        boundStatement.setDouble("volume", row.volume)
-        boundStatement.setDouble("adjclose", row.adjclose)
-        
-        val rs = cassandraSession.execute(boundStatement)
-        (row.date, row.symbol, rs.wasApplied())
-    }
-    val elapsed = System.currentTimeMillis() - startTime
-    logger.warn(s"Partition size: ${results.length}, taken: $elapsed ms")
-    cassandraSession.close()
-    results.toIterator
-  }
-  
-  def toStock(row:Row) = {
-    Stock(row.getTimestamp(0)
-         , row.getDouble(1)
-         , row.getDouble(2)
-         , row.getDouble(3)
-         , row.getDouble(4)
-         , row.getDouble(5)
-         , row.getDouble(6)
-         , row.getString(7))
-  }
-
-  
-
-  def main(args: Array[String]) {
-    
-    val numPartitions = 10
-    
-    println("Spark Web UI: " + spark.sparkContext.uiWebUrl)
-    loadAs("/home/training/Downloads/datasets/stocks.csv", "stocks")
-    
-    import spark.implicits._
-    val stocks = spark.table("stocks").map(toStock).coalesce(numPartitions) //.repartition(Functions.col("date"))
-    stocks.select("date").distinct().show()
-
-    val results = stocks.mapPartitions(saveEachPartition).toDF("date", "symbol", "applied")
-    results.show()
-    results.coalesce(1).write.mode(SaveMode.Overwrite).csv("save-results")
-    spark.close()
-  }
+		return allRows;
+		
+	}
+	
+	/*create table stocks(date date, open double,high double,low double,close double,volume double,
+	 * adjclose double,symbol text, primary key((symbol), date)) with clustering order by (date DESC);*/
+	
+	public static void main(String[] args) throws Exception {
+		Cluster cluster = Cluster
+				.builder()
+				.addContactPoint("localhost")
+				.build();
+		session = cluster.connect();
+		String sql = "insert into demo.stocks (date, open, high, low, close, volume, adjclose, symbol) values (?, ?, ?, ?, ?, ?, ?, ?)";
+		loadStatement = session.prepare(sql);
+		SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
+		List<String[]> rows = readData("/home/training/Downloads/datasets/stocks.csv");
+		for(String[] tokens: rows) {
+			if(!tokens[0].startsWith("date")) {
+				
+				Date date = simpleDateFormat.parse(tokens[0]);
+				LocalDate localDate = LocalDate.fromMillisSinceEpoch(date.getTime());
+				Double open = Double.valueOf(tokens[1]);
+				Double high= Double.valueOf(tokens[2]); 
+				Double low= Double.valueOf(tokens[3]);
+				Double close= Double.valueOf(tokens[4]); 
+				Double volume= Double.valueOf(tokens[5]);
+				Double adjclose = Double.valueOf(tokens[6]); 
+				String symbol = tokens[7];
+				
+				session.execute(loadStatement.bind(localDate, open, high, low, close, volume, adjclose, symbol));
+			}
+		}
+		session.close();
+		cluster.close();
+	}
 
 }
